408597b5d680215e7baa3942d1b7eb802704d14b
Lin zechao <2801034065@qq.com>
2021-08-05 10:31:25 +0800

    update

--- week1-intro/assignment1/assignment of zechao.py
+++ week1-intro/assignment1/assignment of zechao.py
@@ -5,8 +5,14 @@
 
 # Q1. Write a program which can compute the factorial of a given numbers.
 
-
 def factorial(x: int) -> int:
+    if x >= 0:
+        sum = 1
+        for i in range(1, x + 1):
+            sum = sum * i
+        return sum
+    elif x < 0:
+        print("请输入一个正数")
     return 1
 
 
@@ -19,7 +25,10 @@ assert factorial(9) == 362880
 # [1 + 2 + ... + x] and x is always >= 1.
 
 def print_sum(x: int) -> str:
-    return ""
+    sum = 0
+    for i in range(1, x + 1):
+        sum += i
+    return str(sum)
 
 
 assert print_sum(1) == "1"
@@ -30,7 +39,8 @@ assert print_sum(5) == "15"
 # Q3. Write a program to check is a year is leap year (x is always > 0)
 
 def is_leap_year(year: int) -> bool:
-    return False
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+
 
 
 assert is_leap_year(2000)
@@ -42,7 +52,7 @@ assert not is_leap_year(2001)
 # Q4. Write a program to convert a list of lowercase words to uppercase words.
 
 def to_upper_case(words: [str]) -> [str]:
-    return []
+    return [i.upper() for i in words]
 
 
 assert to_upper_case(["abc", "de"]) == ["ABC", "DE"]
@@ -53,7 +63,10 @@ assert to_upper_case(["Amazon", "Apple"]) == ["AMAZON", "APPLE"]
 # https://baike.baidu.com/item/%E5%BC%82%E6%88%96/10993677?fromtitle=xor&fromid=64178
 
 def xor(a: bool, b: bool) -> bool:
-    return False
+    if a != b:
+        return True
+    else:
+        return False
 
 
 assert not xor(True, True)
@@ -72,5 +85,16 @@ assert "T" in get_current_time()
 assert "Z" in get_current_time()
 assert 20 == len(get_current_time())
 
+
 # Q7. Write a Python program to sum of two given integers. However, if the sum is between 15 to 20 it will return 20
 # please define function and test yourself.
+def sum_int(a: int, b: int) -> int:
+    if 15 <= a + b <= 20:
+        return 20
+    else:
+        return a + b
+
+
+assert sum_int(10, 10)
+assert sum_int(20, 10)
+assert sum_int(10, 7)
