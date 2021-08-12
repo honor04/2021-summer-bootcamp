b9a9de1039058e3f841e77321b2608c312456977
Your Name <honor04@qq.com>
2021-08-12 19:12:07 +0800

    finish my pandas mini project
    I found that cost[-1] and cost[i-1] do the same.

--- week3-numpy-pandas/mini-project-1/scheduled_investment_HaoChen.py
+++ week3-numpy-pandas/mini-project-1/scheduled_investment_HaoChen.py
@@ -7,7 +7,7 @@ import pandas as pd
 import datetime as dt
 
 RAW_DATA_NAME = "data/QQQ.csv"
-ANALYSIS_RESULT_DATA_NAME = "data/QQQ-result.csv"
+ANALYSIS_RESULT_DATA_NAME = "data/HaoChen_QQQ_result.csv"
 
 
 def read_data() -> pd.DataFrame:
@@ -45,8 +45,10 @@ def calculate_scheduled_investment(data: pd.DataFrame) -> ():
         #   然后总需要根据open_price计算asset, 并且加入assets
         if is_monday(date):
             positions.append(positions[-1] + 10)
+            cost.append(cost[-1] + open_price * shares)
         else:
             positions.append(positions[-1])
+            cost.append(cost[-1])
         assets.append(open_price * positions[-1])
     print(len(positions))
     return positions, cost, assets
@@ -61,9 +63,13 @@ def export_result() -> float:
     # 在这里调用 calculate_scheduled_investment, 并且赋值
     # 到asset 和cost.
     # 最后返回十年的年化率
-    asset = [1]  # replace
-    cost = [1]  # replace
-    return annual_return(10, asset[-1] / cost[-1])  # 10 years
+    data = read_data()
+    positions, cost, assets = calculate_scheduled_investment(data)
+    data["positions"] = positions
+    data["asset"] = assets
+    data["cost"] = cost
+    write_data(data)
+    return annual_return(10, assets[-1] / cost[-1])  # 10 years
 
 
 # -- TODO: Part 2 (END)
@@ -71,4 +77,4 @@ def export_result() -> float:
 
 if __name__ == '__main__':
     print(calculate_scheduled_investment(read_data()))
-    # print("Investment Return: ", export_result())
+    print("Investment Return: ", export_result())
